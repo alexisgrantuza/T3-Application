ba9cc1cba9b567dc959dceb466d93fec9dc5f351
Jhoana <diasantajhoana@gmail.com>
2025-11-11 21:15:13 +0800

    update

--- src/app/Landingpage/page.tsx
+++ src/app/Landingpage/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import HeroSection from "./components/herosection";
+import HowItWorks from "./components/howitworks";
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <HowItWorks />
     </>
   );
 }
